2cba9f670bdeb15a8823e9daae0a66a9e9adce9a
David Guttman <david@davidguttman.com>
2018-07-21 08:59:15 -0700

    misc: update example to export to csv

--- example.js
+++ example.js
@@ -1,10 +1,37 @@
-require('replay')
-// .mode = 'record'
+require('replay').mode = 'record'
+
+var ss = require('serialize-stream')
+var pump = require('pump')
+var intoStream = require('into-stream').obj
 
 var optimus = require('./')
 
-optimus({symbol: 'AAPL'}, function (err, data) {
+var symbol = process.argv[2]
+if (!symbol) {
+  console.error('Please provide symbol')
+  process.exit(1)
+}
+
+optimus({symbol}, function (err, data) {
   if (err) return console.error(err)
 
-  console.log('data', data)
+  var headers = [
+    'type',
+    'expirationDate',
+    'strike',
+    'lastPrice',
+    'bid',
+    'ask',
+    'change',
+    'changePercent',
+    'volume',
+    'openInterest',
+    'impliedVolatility',
+    'lastTradeDate',
+    'contractName'
+  ]
+
+  pump(intoStream(data), ss('csv', {headers}), process.stdout, function (err) {
+    if (err) return console.error(err)
+  })
 })
